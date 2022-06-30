ee8fa044ffcce4f6f248bdec2f85e3ba525f6872
msantanal <108295791+msantanal@users.noreply.github.com>
2022-06-30 18:22:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,35 +1,11 @@
-data "azurerm_client_config" "current" {}
-
-data "azurerm_resource_group" "rg" {
-  name      = var.rg_name
-  location  = var.rg_location
+data "azurerm_resource_group" "RG" {
+  name = var.resource_group_name
 }
 
-resource "azurerm_key_vault" "key" {
-  name                        = var.key_vault_name
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-
-  sku_name = "standard"
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "Get",
-    ]
-
-    secret_permissions = [
-      "Get",
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
-  }
+resource"azurerm_key_vault""KEYVAULT" {
+  name                = var.name_keyvault
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.RG.name
+  tenant_id           = var.tenant_id
+  sku_name            = var.sku_keyvault
 }
